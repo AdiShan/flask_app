f527107a8082bb15eb0406cab2f167ea032c063c
AdiShan <aditya@adappt.com>
2020-10-05 16:58:58 +0530

    updated sh scripts

--- ap_to_station_mode.sh
+++ ap_to_station_mode.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-mv /etc/dhcpcd.conf /etc/dhcpcd.conf.ap
-mv /etc/dhcpcd.conf.station /etc/dhcpcd.conf
+sudo mv /etc/dhcpcd.conf.station /etc/dhcpcd.conf
+sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.ap
+sleep 1
 sudo systemctl stop dnsmasq
+sleep 1
 sudo systemctl stop hostapd
+sleep 1
 sudo systemctl restart dhcpcd
--- station_to_ap_mode.sh
+++ station_to_ap_mode.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-mv /etc/dhcpcd.conf /etc/dhcpcd.conf.station
-mv /etc/dhcpcd.conf.ap /etc/dhcpcd.conf
-sudo systemctl start hostapd
-sudo systemctl start dnsmasq
+sudo mv /etc/dhcpcd.conf.ap /etc/dhcpcd.conf
+sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.station
+sleep 1
 sudo systemctl restart dhcpcd
+sleep 1
+sudo systemctl start hostapd
+sleep 1
+sudo systemctl start dnsmasq
+
